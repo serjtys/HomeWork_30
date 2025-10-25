56e4784ac065a794a4a099fa8adef23c40da917d
serj <sergejtustin601@gmail.com>
2025-10-26 00:17:34 +0300

    homework-35-2 fix v2

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
-if not ALLOWED_HOSTS[0]:
-    ALLOWED_HOSTS = ['51.250.20.114', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS_STR = os.getenv('ALLOWED_HOSTS', '')
+if ALLOWED_HOSTS_STR and ALLOWED_HOSTS_STR != 'False':
+    ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS_STR.split(',')]
+else:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
